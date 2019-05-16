9a56b3777b1e55de0d80eb2ba85be9ccbb4bf2f5
Wendall Cada <wendallc@83864.com>
2019-05-16 06:31:57 -0700

    Change item damage (crazy internal MC thing) to durability. Added mainhand/offhand damage and speed.

--- src/main/java/simpletextoverlay/tag/TagPlayerEquipment.java
+++ src/main/java/simpletextoverlay/tag/TagPlayerEquipment.java
@@ -1,12 +1,25 @@
 package simpletextoverlay.tag;
 
+import com.google.common.collect.Multimap;
+
+import java.util.Map.Entry;
+import java.util.UUID;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 
 import simpletextoverlay.client.gui.overlay.InfoItem;
 import simpletextoverlay.tag.registry.TagRegistry;
+import simpletextoverlay.SimpleTextOverlay;
 import simpletextoverlay.util.EntityHelper;
 
 public abstract class TagPlayerEquipment extends Tag {
@@ -21,6 +34,9 @@ public abstract class TagPlayerEquipment extends Tag {
     public TagPlayerEquipment(final int slot) {
         this.slot = slot;
     }
+
+    private static final UUID ATTACK_DAMAGE_MODIFIER = UUID.fromString("CB3F55D3-645C-4F38-A497-9C13A33DB5CF");
+    private static final UUID ATTACK_SPEED_MODIFIER = UUID.fromString("FA233E1C-4180-4865-B01B-BCCE9785ACA3");
 
     @Override
     public String getCategory() {
@@ -87,8 +103,57 @@ public abstract class TagPlayerEquipment extends Tag {
         }
     }
 
-    public static class Damage extends TagPlayerEquipment {
-        public Damage(final int slot) {
+    public static class AttackDamage extends TagPlayerEquipment {
+        public AttackDamage(final int slot) {
+            super(slot);
+        }
+
+        @Override
+        public String getValue() {
+            final ItemStack itemStack = getItemStack(this.slot);
+            String attackDamage = "";
+
+            Multimap<String, AttributeModifier> modifierMap = itemStack.getAttributeModifiers(EntityEquipmentSlot.MAINHAND);
+            for (Entry<String, AttributeModifier> entry : modifierMap.entries()) {
+                AttributeModifier attributeModifier = entry.getValue();
+                if (attributeModifier.getID().equals(ATTACK_DAMAGE_MODIFIER)) {
+                    double damageModifier = attributeModifier.getAmount();
+                    damageModifier = damageModifier + player.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getBaseValue();
+                    damageModifier = damageModifier + (double)EnchantmentHelper.getModifierForCreature(itemStack, EnumCreatureAttribute.UNDEFINED);
+                    attackDamage = String.format("%.1f", Math.round(damageModifier * 10.0) / 10.0);
+                }
+            }
+
+            return attackDamage;
+        }
+    }
+
+    public static class AttackSpeed extends TagPlayerEquipment {
+        public AttackSpeed(final int slot) {
+            super(slot);
+        }
+
+        @Override
+        public String getValue() {
+            final ItemStack itemStack = getItemStack(this.slot);
+            String attackSpeed = "";
+
+            Multimap<String, AttributeModifier> modifierMap = itemStack.getAttributeModifiers(EntityEquipmentSlot.MAINHAND);
+            for (Entry<String, AttributeModifier> entry : modifierMap.entries()) {
+                AttributeModifier attributeModifier = entry.getValue();
+                if (attributeModifier.getID().equals(ATTACK_SPEED_MODIFIER)) {
+                    double speedModifier = attributeModifier.getAmount();
+                    speedModifier += player.getEntityAttribute(SharedMonsterAttributes.ATTACK_SPEED).getBaseValue();
+                    attackSpeed = String.format("%.2f", Math.round(speedModifier * 100.0) / 100.0);
+                }
+            }
+
+            return attackSpeed;
+        }
+    }
+
+    public static class Durability extends TagPlayerEquipment {
+        public Durability(final int slot) {
             super(slot);
         }
 
@@ -103,8 +168,8 @@ public abstract class TagPlayerEquipment extends Tag {
         }
     }
 
-    public static class MaximumDamage extends TagPlayerEquipment {
-        public MaximumDamage(final int slot) {
+    public static class MaximumDurability extends TagPlayerEquipment {
+        public MaximumDurability(final int slot) {
             super(slot);
         }
 
@@ -119,8 +184,8 @@ public abstract class TagPlayerEquipment extends Tag {
         }
     }
 
-    public static class DamageLeft extends TagPlayerEquipment {
-        public DamageLeft(final int slot) {
+    public static class DurabilityRemaining extends TagPlayerEquipment {
+        public DurabilityRemaining(final int slot) {
             super(slot);
         }
 
@@ -132,22 +197,6 @@ public abstract class TagPlayerEquipment extends Tag {
             }
 
             return String.valueOf(itemStack.isItemStackDamageable() ? itemStack.getMaxDamage() - itemStack.getItemDamage() : 0);
-        }
-    }
-
-    public static class Quantity extends TagPlayerEquipment {
-        public Quantity(final int slot) {
-            super(slot);
-        }
-
-        @Override
-        public String getValue() {
-            final ItemStack itemStack = getItemStack(this.slot);
-            if (itemStack.isEmpty()) {
-                return String.valueOf(0);
-            }
-
-            return String.valueOf(EntityHelper.getItemCountInInventory(player.inventory, itemStack.getItem(), itemStack.getItemDamage()));
         }
     }
 
@@ -169,13 +218,18 @@ public abstract class TagPlayerEquipment extends Tag {
     }
 
     public static void register() {
+        for (int i = 0; i < 2; i++) {
+            TagRegistry.INSTANCE.register(new AttackDamage(SLOTS[i]).setName(TYPES[i] + "attackdamage"));
+            TagRegistry.INSTANCE.register(new AttackDamage(SLOTS[i]).setName(TYPES[i] + "attackdamage"));
+            TagRegistry.INSTANCE.register(new AttackSpeed(SLOTS[i]).setName(TYPES[i] + "attackspeed"));
+            TagRegistry.INSTANCE.register(new AttackSpeed(SLOTS[i]).setName(TYPES[i] + "attackspeed"));
+        }
         for (int i = 0; i < TYPES.length; i++) {
             TagRegistry.INSTANCE.register(new Name(SLOTS[i]).setName(TYPES[i] + "name"));
             TagRegistry.INSTANCE.register(new UniqueName(SLOTS[i]).setName(TYPES[i] + "uniquename"));
-            TagRegistry.INSTANCE.register(new Damage(SLOTS[i]).setName(TYPES[i] + "damage"));
-            TagRegistry.INSTANCE.register(new MaximumDamage(SLOTS[i]).setName(TYPES[i] + "maxdamage"));
-            TagRegistry.INSTANCE.register(new DamageLeft(SLOTS[i]).setName(TYPES[i] + "damageleft"));
-            TagRegistry.INSTANCE.register(new Quantity(SLOTS[i]).setName(TYPES[i] + "quantity"));
+            TagRegistry.INSTANCE.register(new Durability(SLOTS[i]).setName(TYPES[i] + "durability"));
+            TagRegistry.INSTANCE.register(new MaximumDurability(SLOTS[i]).setName(TYPES[i] + "maxdurability"));
+            TagRegistry.INSTANCE.register(new DurabilityRemaining(SLOTS[i]).setName(TYPES[i] + "durabilityremaining"));
             TagRegistry.INSTANCE.register(new Icon(SLOTS[i], false).setName(TYPES[i] + "icon"));
             TagRegistry.INSTANCE.register(new Icon(SLOTS[i], true).setName(TYPES[i] + "largeicon"));
         }
