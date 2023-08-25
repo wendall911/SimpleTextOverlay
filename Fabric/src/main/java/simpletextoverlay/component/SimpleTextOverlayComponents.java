4e5acb8ed75a48f2ac255a8a811c671a0e39866e
Wendall Cada <wendallc@83864.com>
2023-08-25 13:47:02 -0700

    Initial HUD Compass Fabric implementation.

---
+++ Fabric/src/main/java/simpletextoverlay/component/SimpleTextOverlayComponents.java
@@ -0,0 +1,27 @@
+package simpletextoverlay.component;
+
+import dev.onyxstudios.cca.api.v3.component.ComponentKey;
+import dev.onyxstudios.cca.api.v3.component.ComponentRegistry;
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
+import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
+
+import net.minecraft.resources.ResourceLocation;
+
+import simpletextoverlay.SimpleTextOverlay;
+
+public class SimpleTextOverlayComponents implements EntityComponentInitializer {
+
+    public static ResourceLocation STO_DATA = new ResourceLocation(SimpleTextOverlay.MODID, "sto_data");
+
+    public static final ComponentKey<ComponentDataManager> DATA_MANAGER = ComponentRegistry.getOrCreate(
+        new ResourceLocation(SimpleTextOverlay.MODID, "sto_provider"),
+        ComponentDataManager.class
+    );
+
+    @Override
+    public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
+        registry.registerForPlayers(DATA_MANAGER, player -> new ComponentDataManager(), RespawnCopyStrategy.ALWAYS_COPY);
+    }
+
+}
