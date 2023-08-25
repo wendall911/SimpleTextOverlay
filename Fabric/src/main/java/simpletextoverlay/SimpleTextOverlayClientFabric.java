c5fd2805db53d3e1b003f594b115233b259a693d
Wendall Cada <wendallc@83864.com>
2023-08-25 13:47:02 -0700

    Initial HUD Compass Fabric implementation.

--- Fabric/src/main/java/simpletextoverlay/SimpleTextOverlayClientFabric.java
+++ Fabric/src/main/java/simpletextoverlay/SimpleTextOverlayClientFabric.java
@@ -1,9 +1,14 @@
 package simpletextoverlay;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
+import net.minecraft.client.Minecraft;
+
+import simpletextoverlay.component.SimpleTextOverlayComponents;
 import simpletextoverlay.event.GameOverlayEventHandler;
 import simpletextoverlay.overlay.OverlayManager;
+import simpletextoverlay.platform.Services;
 
 public class SimpleTextOverlayClientFabric implements ClientModInitializer {
 
@@ -11,6 +16,12 @@ public class SimpleTextOverlayClientFabric implements ClientModInitializer {
     public void onInitializeClient() {
         GameOverlayEventHandler.init();
         OverlayManager.INSTANCE.init();
+
+        ClientPlayNetworking.registerGlobalReceiver(SimpleTextOverlayComponents.STO_DATA, (client, handler, buf, responseSender) -> {
+            client.execute(() -> Services.CAPABILITY_PLATFORM.getDataManagerCapability(Minecraft.getInstance().player).ifPresent(data -> {
+                data.read(buf);
+            }));
+        });
     }
 
 }
--- Fabric/src/main/java/simpletextoverlay/platform/FabricCapabilityPlatform.java
+++ Fabric/src/main/java/simpletextoverlay/platform/FabricCapabilityPlatform.java
@@ -1,23 +1,32 @@
 package simpletextoverlay.platform;
 
+import java.util.Optional;
+
+import io.netty.buffer.Unpooled;
+
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 
+import simpletextoverlay.component.SimpleTextOverlayComponents;
+import simpletextoverlay.network.SyncData;
 import simpletextoverlay.overlay.compass.DataManager;
 import simpletextoverlay.platform.services.ICapabilityPlatform;
-
-import java.util.Optional;
 
 public class FabricCapabilityPlatform implements ICapabilityPlatform {
 
     @Override
     public Optional<? extends DataManager> getDataManagerCapability(Player player) {
-        return Optional.empty();
+        return SimpleTextOverlayComponents.DATA_MANAGER.maybeGet(player);
     }
 
     @Override
     public void syncData(ServerPlayer sp) {
+        SyncData syncData = new SyncData();
+        FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.wrappedBuffer(syncData.bytes));
 
+        ServerPlayNetworking.send(sp, SimpleTextOverlayComponents.STO_DATA, buf);
     }
 
 }
