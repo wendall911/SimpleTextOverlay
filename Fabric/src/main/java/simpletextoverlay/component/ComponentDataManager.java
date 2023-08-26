c91461e22b67eaf63a239d60a6314d0bc5e1099c
Wendall Cada <wendallc@83864.com>
2023-08-26 13:38:28 -0700

    Add event handlng for Fabric.

--- Fabric/src/main/java/simpletextoverlay/component/ComponentDataManager.java
+++ Fabric/src/main/java/simpletextoverlay/component/ComponentDataManager.java
@@ -3,13 +3,20 @@ package simpletextoverlay.component;
 import dev.onyxstudios.cca.api.v3.component.Component;
 
 import net.fabricmc.fabric.api.util.NbtType;
+
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.world.entity.player.Player;
+
 import simpletextoverlay.overlay.compass.DataManager;
 
 public class ComponentDataManager extends DataManager implements Component {
 
     private final DataManager instance = new DataManager();
+
+    public ComponentDataManager(Player player) {
+        instance.setPlayer(player);
+    }
 
     @Override
     public void writeToNbt(CompoundTag tag) {
