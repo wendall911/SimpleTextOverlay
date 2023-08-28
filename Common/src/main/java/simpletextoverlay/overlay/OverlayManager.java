fad1c59dda3db7df9fac0eb11ab7606bb7810f36
Wendall Cada <wendallc@83864.com>
2023-08-28 15:46:06 -0700

    Add support for Fabric Seasons Remove redundant Serene Seasons Info conditional.

--- Common/src/main/java/simpletextoverlay/overlay/OverlayManager.java
+++ Common/src/main/java/simpletextoverlay/overlay/OverlayManager.java
@@ -46,7 +46,7 @@ public class OverlayManager {
                     lines.add(new DaysInfo(OverlayConfig.daysLabel(), lineNum));
                     break;
                 case "season":
-                    if (Services.PLATFORM.isModLoaded("sereneseasons")) {
+                    if (Services.PLATFORM.isModLoaded("sereneseasons") || Services.PLATFORM.isModLoaded("seasons")) {
                         lines.add(new SeasonInfo("", lineNum));
                     }
                     else {
