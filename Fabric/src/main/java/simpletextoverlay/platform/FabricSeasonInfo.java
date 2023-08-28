cf5ca169d1c7e380607df16048ae693d92f00d9e
Wendall Cada <wendallc@83864.com>
2023-08-28 15:46:06 -0700

    Add support for Fabric Seasons Remove redundant Serene Seasons Info conditional.

--- Fabric/src/main/java/simpletextoverlay/platform/FabricSeasonInfo.java
+++ Fabric/src/main/java/simpletextoverlay/platform/FabricSeasonInfo.java
@@ -2,10 +2,14 @@ package simpletextoverlay.platform;
 
 import com.mojang.datafixers.util.Pair;
 
+import io.github.lucaargolo.seasons.FabricSeasons;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.Level;
 
+import simpletextoverlay.SimpleTextOverlay;
+import simpletextoverlay.config.OverlayConfig;
 import simpletextoverlay.platform.services.ISeasonInfo;
 import simpletextoverlay.util.SubSeason;
 
@@ -13,6 +17,18 @@ public class FabricSeasonInfo implements ISeasonInfo {
 
     @Override
     public Pair<Component, SubSeason> getSeasonName(Level level, BlockPos pos) {
+        if (OverlayConfig.hasSeasonDimension(level.dimension().location().toString())) {
+            try {
+                SubSeason subSeason = SubSeason.valueOf(FabricSeasons.getCurrentSeason().name());
+                Component seasonName = Component.translatable("desc." + SimpleTextOverlay.MODID + "." + subSeason.name().toLowerCase());
+
+                return Pair.of(seasonName, subSeason);
+            }
+            catch (IllegalArgumentException e) {
+                SimpleTextOverlay.LOGGER.debug("Unable to convert subseason {}", e);
+            }
+        }
+
         return null;
     }
 
