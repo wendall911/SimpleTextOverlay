d53fd6cdc2df225cffbb2a547cc54b872be674c0
Wendall Cada <wendallc@83864.com>
2024-06-12 16:03:45 -0700

    Reset pins cache. Removes bad pins on changing worlds.

--- Fabric/src/main/java/simpletextoverlay/SimpleTextOverlayFabric.java
+++ Fabric/src/main/java/simpletextoverlay/SimpleTextOverlayFabric.java
@@ -1,11 +1,20 @@
 package simpletextoverlay;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
+import simpletextoverlay.overlay.compass.DataManager;
 
 public class SimpleTextOverlayFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
+            DataManager.resetCache();
+        });
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+            DataManager.resetCache();
+        });
     }
 
 }
