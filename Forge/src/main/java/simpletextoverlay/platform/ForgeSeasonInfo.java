0456afde69aafe975444f59d7788e6c55f951773
Wendall Cada <wendallc@83864.com>
2024-08-09 10:29:14 -0700

    Fix season label after Serene Season API breaking change.

--- Forge/src/main/java/simpletextoverlay/platform/ForgeSeasonInfo.java
+++ Forge/src/main/java/simpletextoverlay/platform/ForgeSeasonInfo.java
@@ -20,7 +20,7 @@ public class ForgeSeasonInfo implements ISeasonInfo {
     @Override
     public Pair<Component, SubSeason> getSeasonName(Level level, BlockPos pos) {
         if (BiomeConfig.enablesSeasonalEffects(Objects.requireNonNull(level).getBiome(pos))) {
-            SubSeason subSeason = SubSeason.getSubSeason(level, SeasonHelper.getSeasonState(level).getSeasonDuration());
+            SubSeason subSeason = SubSeason.values()[SeasonHelper.getSeasonState(level).getSubSeason().ordinal()];
             Component seasonName = Component.translatable("desc." + SimpleTextOverlay.MODID + "." + subSeason.name().toLowerCase());
 
             return Pair.of(seasonName, subSeason);
