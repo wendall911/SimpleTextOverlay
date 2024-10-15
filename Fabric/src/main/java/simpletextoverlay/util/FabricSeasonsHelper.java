8e1a7e2946d10af288652298c27f0f2b9486de7f
Wendall Cada <wendallc@83864.com>
2024-10-15 09:56:32 -0700

    Add back Fabric Seasons support!

--- Fabric/src/main/java/simpletextoverlay/util/FabricSeasonsHelper.java
+++ Fabric/src/main/java/simpletextoverlay/util/FabricSeasonsHelper.java
@@ -1,21 +1,18 @@
 package simpletextoverlay.util;
 
-//import static io.github.lucaargolo.seasons.FabricSeasons.CONFIG;
+import static io.github.lucaargolo.seasons.FabricSeasons.CONFIG;
 
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
-import simpletextoverlay.config.OverlayConfig;
 
 public class FabricSeasonsHelper {
-    public static int getSeasonDuration() {
-        // Hardcoding for now so this  will work if they update
-        //return CONFIG.getSeasonLength();
-        return 672000;
 
+    public static int getSeasonDuration() {
+        return CONFIG.getSpringLength();
     }
 
     public static boolean isDimensionWhitelisted(ResourceKey<Level> dimension) {
-        return OverlayConfig.hasSeasonDimension(dimension.location().toString());
+        return CONFIG.isValidInDimension(dimension);
     }
 
 }
