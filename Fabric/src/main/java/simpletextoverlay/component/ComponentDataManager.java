c0b9b427f6d44ec0635b4fb96b9ad6ebdd0ccd6e
Wendall Cada <wendallc@83864.com>
2025-08-12 11:42:45 -0700

    Not sure what to do with the new serialization stuff. Need a few days to work on this, have no clue how this impacts what I'm doing.

--- Fabric/src/main/java/simpletextoverlay/component/ComponentDataManager.java
+++ Fabric/src/main/java/simpletextoverlay/component/ComponentDataManager.java
@@ -27,14 +27,18 @@ public class ComponentDataManager extends DataManager implements Component {
 
     @Override
     public void writeData(ValueOutput writeView) {
+        /* WTF CCA?
         tag.put("Data", instance.write());
+         */
     }
 
     @Override
     public void readData(ValueInput readView) {
+        /* WTF CCA?
         Optional<ListTag> nbt = tag.getList("Data", NbtType.COMPOUND);
 
         instance.read(nbt);
+         */
     }
 
 }
