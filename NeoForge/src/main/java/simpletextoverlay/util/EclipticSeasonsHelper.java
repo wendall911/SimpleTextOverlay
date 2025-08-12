afb42dc13a6a5881240cc7f78a70ab102305bfa9
Wendall Cada <wendallc@83864.com>
2025-08-12 09:07:32 -0700

    Use new Ecliptic Seasons api for getting solar term.

--- NeoForge/src/main/java/simpletextoverlay/util/EclipticSeasonsHelper.java
+++ NeoForge/src/main/java/simpletextoverlay/util/EclipticSeasonsHelper.java
@@ -1,20 +1,20 @@
 package simpletextoverlay.util;
 
 import com.teamtea.eclipticseasons.api.constant.solar.SolarTerm;
-import com.teamtea.eclipticseasons.common.handler.SolarUtil;
+import com.teamtea.eclipticseasons.api.util.EclipticUtil;
 
 import net.minecraft.world.level.Level;
 
 public class EclipticSeasonsHelper {
 
     public static boolean isSeasonDimension(Level level) {
-        SolarTerm solarTerm = SolarUtil.getSolarTerm(level);
+        SolarTerm solarTerm = EclipticUtil.INSTANCE.getSolarTerm(level);
 
         return solarTerm != SolarTerm.NONE;
     }
 
     public static SubSeason getSubSeason(Level level) {
-        SolarTerm solarTerm = SolarUtil.getSolarTerm(level);
+        SolarTerm solarTerm = EclipticUtil.INSTANCE.getSolarTerm(level);
 
         return SubSeason.values()[(solarTerm.ordinal() / 2)];
     }
