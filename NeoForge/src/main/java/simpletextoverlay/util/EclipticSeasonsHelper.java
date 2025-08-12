4e9910030abe8a9896759441d497c095f34046a0
Wendall Cada <wendallc@83864.com>
2025-08-12 09:22:41 -0700

    Temporarily disable Ecliptic Seasons support until mod is updated to 1.21.2+

--- NeoForge/src/main/java/simpletextoverlay/util/EclipticSeasonsHelper.java
+++ NeoForge/src/main/java/simpletextoverlay/util/EclipticSeasonsHelper.java
@@ -1,22 +1,31 @@
 package simpletextoverlay.util;
 
+/*
+ * Temporarily disabled due to Ecliptic Seasons not being updated to 1.21.2+
 import com.teamtea.eclipticseasons.api.constant.solar.SolarTerm;
 import com.teamtea.eclipticseasons.api.util.EclipticUtil;
+ */
 
 import net.minecraft.world.level.Level;
 
 public class EclipticSeasonsHelper {
 
     public static boolean isSeasonDimension(Level level) {
+        /*
         SolarTerm solarTerm = EclipticUtil.INSTANCE.getSolarTerm(level);
 
         return solarTerm != SolarTerm.NONE;
+         */
+        return false;
     }
 
     public static SubSeason getSubSeason(Level level) {
+        /*
         SolarTerm solarTerm = EclipticUtil.INSTANCE.getSolarTerm(level);
 
         return SubSeason.values()[(solarTerm.ordinal() / 2)];
+         */
+        return SubSeason.MID_SPRING;
     }
 
 }
