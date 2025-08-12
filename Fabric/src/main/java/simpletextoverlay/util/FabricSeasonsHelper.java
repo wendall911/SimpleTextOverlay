ca6a7b1b1cb4ef08fb24ba1ead51abb626d4216f
Wendall Cada <wendallc@83864.com>
2025-08-12 11:42:29 -0700

    Temporarily disable fabric seasons.

--- Fabric/src/main/java/simpletextoverlay/util/FabricSeasonsHelper.java
+++ Fabric/src/main/java/simpletextoverlay/util/FabricSeasonsHelper.java
@@ -1,6 +1,9 @@
 package simpletextoverlay.util;
 
-import static io.github.lucaargolo.seasons.FabricSeasons.CONFIG;
+/*
+ * Wait until Fabric Seasons is available
+ */
+//import static io.github.lucaargolo.seasons.FabricSeasons.CONFIG;
 
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
@@ -8,11 +11,13 @@ import net.minecraft.world.level.Level;
 public class FabricSeasonsHelper {
 
     public static int getSeasonDuration() {
-        return CONFIG.getSpringLength();
+        //return CONFIG.getSpringLength();
+        return 0; // Placeholder, as Fabric Seasons is not implemented yet
     }
 
     public static boolean isDimensionWhitelisted(ResourceKey<Level> dimension) {
-        return CONFIG.isValidInDimension(dimension);
+        //return CONFIG.isValidInDimension(dimension);
+        return false; // Placeholder, as Fabric Seasons is not implemented yet
     }
 
 }
