4c28bdd6f55e4d860ede4708c426700c8c139226
Wendall Cada <wendallc@83864.com>
2025-12-18 09:38:49 -0800

    Fix Fabric support for Homeostatic Seasons.

--- Fabric/src/main/java/simpletextoverlay/platform/FabricSeasonInfo.java
+++ Fabric/src/main/java/simpletextoverlay/platform/FabricSeasonInfo.java
@@ -21,8 +21,11 @@ public class FabricSeasonInfo implements ISeasonInfo {
         SubSeason subSeason = SubSeason.MID_SPRING;
 
         if (Services.PLATFORM.isModLoaded("homeostaticseasons")) {
-            subSeason = HomeostaticSeasonsHelper.getSubSeason(level);
             hasSeasonDimension = HomeostaticSeasonsHelper.isDimensionWhitelisted(level.dimension());
+
+            if (hasSeasonDimension) {
+                subSeason = HomeostaticSeasonsHelper.getSubSeason(level);
+            }
         }
         else if (Services.PLATFORM.isModLoaded("sereneseasons")) {
             subSeason = SereneSeasonsFabricHelper.getSubSeason(level);
